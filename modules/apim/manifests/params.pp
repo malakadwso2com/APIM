10996d79085fb223688646b29e06dae68feb02b7
Malakadwso2com <malakad@wso2.com>
2021-02-01 12:41:38 +0530

    testing

--- modules/apim/manifests/params.pp
+++ modules/apim/manifests/params.pp
@@ -42,6 +42,7 @@ class apim::params inherits apim_common::params {
   $hostname = 'api.am.wso2.com'
 
   # ----- api-manager.xml config params -----
-  $oauth_configs_revoke_api_url = 'https://api.am.wso2.com:${https.nio.port}/revoke'
+  #$oauth_configs_revoke_api_url = 'https://api.am.wso2.com:${https.nio.port}/revoke'
+  $oauth_configs_revoke_api_url = 'https://api.am.wso2.com/revoke'
   $throttle_config_policy_deployer_url = 'https://api.am.wso2.com:${mgt.transport.https.port}${carbon.context}services/'
 }
